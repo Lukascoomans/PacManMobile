343690c874729fcc4d428579aa6b5a8965867481
Lion Gelders <lion.gelders@student.pxl.be>
2018-10-15 14:45:45 +0200

    Work on RecyclerView for Score views
    Worked on the classes that are necessary to properly fill the RecyclerView in the Score views.

--- app/src/main/java/be/pxl/pacmanapp/ScoreListAdapter.java
+++ app/src/main/java/be/pxl/pacmanapp/ScoreListAdapter.java
@@ -1,31 +1,67 @@
 package be.pxl.pacmanapp;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
-public class ScoreListAdapter extends RecyclerView.Adapter<ScoreListAdapter.ScoreListViewHolder> {
-    private int position;
-    private int score;
+public class ScoreListAdapter extends RecyclerView.Adapter<ScoreListViewHolder> {
+    private Cursor cursor;
+    private Context context;
 
-    public ScoreListAdapter(int position, int score){
-        this.position = position;
-        this.score = score;
+    private int position;
+    private int points;
+
+    public ScoreListAdapter(Cursor cursor, Context context){
+        this.cursor = cursor;
+        this.context = context;
     }
 
     @NonNull
     @Override
-    public ScoreListAdapter.ScoreListViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ScoreListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewtype) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.score_list_item, parent,false);
+        return new ScoreListViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ScoreListAdapter.ScoreListViewHolder scoreListViewHolder, int i) {
+    public void onBindViewHolder(@NonNull ScoreListViewHolder viewHolder, int pos) {
+        if(!cursor.moveToPosition(pos)){
+            return;
+        }
 
+        String scorePosition = null;
+        scorePosition = cursor.getColumnName(cursor.getColumnIndex("position"));
+
+        String scorePoints = null;
+        scorePoints = cursor.getColumnName(cursor.getColumnIndex("points"));
+
+        viewHolder.positionTextView.setText(scorePosition);
+        viewHolder.pointsTextView.setText(scorePoints);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if(cursor == null){
+            return 0;
+        }
+
+        return cursor.getCount();
+    }
+
+    public void swapCursor(Cursor newCursor){
+        if(cursor != null){
+            cursor.close();
+        }
+
+        cursor = newCursor;
+
+        if(newCursor != null){
+            this.notifyDataSetChanged();
+        }
     }
 }
