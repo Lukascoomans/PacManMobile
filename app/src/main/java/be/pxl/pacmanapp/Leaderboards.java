d2f9a8903b4592d0c5e35123fe5ce49886ea2992
Lion Gelders <lion.gelders@student.pxl.be>
2018-10-19 08:47:56 +0200

    Slight changes to try out RecyclerView
    Slight changes to try out RecyclerView

--- app/src/main/java/be/pxl/pacmanapp/Leaderboards.java
+++ app/src/main/java/be/pxl/pacmanapp/Leaderboards.java
@@ -1,29 +1,54 @@
 package be.pxl.pacmanapp;
 
+import android.database.Cursor;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOError;
+
 public class Leaderboards extends AppCompatActivity {
-    ListLayoutBinding listLayoutBinding;
-    private List<Score> scoreList;
+    private ScoreListAdapter adapter;
+    private RecyclerView scoreList;
+    private Cursor cursor;
 
-    @Nullable
+    private String sample_response = "[{\"points\":300,\"position\":3},{\"points\":1000,\"position\":2},{\"points\":12300,\"position\":1}]";
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstance){
-        View view = inflater.inflate(R.layout.list_layout, container, false);
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_layout);
 
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.scoreRecyclerView);
+        scoreList = (RecyclerView) this.findViewById(R.id.rv_score);
 
+        cursor = getJSONCursor(sample_response);
 
+        adapter = new ScoreListAdapter(cursor, this);
+
+        scoreList.setLayoutManager(new LinearLayoutManager(this));
+        scoreList.setAdapter(adapter);
     }
 
-    public void fillList(){
-        listLayoutBinding = DataBindingUtil.setContentView();
+    private Cursor getJSONCursor(String response){
+        try
+        {
+            JSONArray array = new JSONArray(response);
+            return new com.example.android.gamescores.JSONArrayCursor(array);
+        }
+        catch(JSONException exception)
+        {
+            String ex = exception.getMessage();
+        }
+        return null;
     }
+
 }
