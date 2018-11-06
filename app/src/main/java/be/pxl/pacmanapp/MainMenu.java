fa08080e219afe5607e12a8d2ab4a720bd40ee35
Lion Gelders <lion.gelders@student.pxl.be>
2018-11-06 12:10:26 +0100

    Revert "Added Initial code for game."
    This reverts commit ba44348657b43fa6761ed3a6512e471cae0f8720.

--- app/src/main/java/be/pxl/pacmanapp/MainMenu.java
+++ app/src/main/java/be/pxl/pacmanapp/MainMenu.java
@@ -1,7 +1,6 @@
 package be.pxl.pacmanapp;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
